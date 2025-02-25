86b5a46942e427f74ad988bd752e5a16da30149e
Ashraf Manna <optimisticashraf@gmail.com>
2025-02-26 00:22:27 +0600

    User data posted and Update operation added for Tasks

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const app=express();
-const port= process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://taskmanager-scic.netlify.app',
+    'https://back-end-omega-livid.vercel.app',
+  ],
+  // methods: "GET, POST, PUT, DELETE, OPTIONS",
+  // allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
+}));
+
 app.use(express.json())
 
 
@@ -27,46 +37,85 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const userCollection = client.db("scicTaskDb").collection("userInfo");
     const taskCollection = client.db("scicTaskDb").collection("allTask");
 
-      // add all task
-      app.post('/addTask', async (req, res) => {
-        const task = req.body;
-        const result = await taskCollection.insertOne(task)
-        res.send(result)
+
+    // userInfo post
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user dosent exist
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user alrady exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+    // add all task
+    app.post('/addTask', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task)
+      res.send(result)
     })
 
     // task get
     app.get('/taskCategory/:category', async (req, res) => {
-        const category = req.params.category
-        const query = { category: category }
-        const result = await taskCollection.find(query).toArray()
-        res.send(result)
+      const category = req.params.category
+      const query = { category: category }
+      const result = await taskCollection.find(query).toArray()
+      res.send(result)
     })
 
     // task get by email
     app.get('/taskEmail/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { email: email }
-        const result = await taskCollection.find(query).toArray()
-        res.send(result)
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await taskCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    //update a task
+    app.put('/allTasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const taskData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true }
+      const updatedTask = {
+
+        // const taskData = { title, description, category: status, time:data.time, updated:time,email:user?.email }
+
+        $set: {
+          title: taskData.title,
+          description: taskData.description,
+          category: taskData.category,
+          time: taskData.time,
+          email: taskData.email,
+          updatedTime: taskData.updated,
+        
+        }
+      }
+
+      const result = await taskCollection.updateOne(filter, updatedTask, options);
+      res.send(result)
     })
 
     // delete a task
     app.delete('/tasks/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-        const result = await taskCollection.deleteOne(query)
-        res.send(result)
+
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
     })
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -75,10 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-  
-app.get("/",(req,res)=>{
- res.send("Server Running")
-})
- 
 
-app.listen(port,()=>{console.log(`server is running at port: ${port}`)})
+app.get("/", (req, res) => {
+  res.send("Server Running")
+})
+
+
+app.listen(port, () => { console.log(`server is running at port: ${port}`) })
